2ce2565ae14a60d0e632bc2de0feae98bba89644
ajorquia <32173620+ajorquia@users.noreply.github.com>
2018-03-17 21:52:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt-get update
+sudo apt-get install fluxgui
