5f379269c1147f9f014232caf1bd079a9894bc8f
ajorquia <32173620+ajorquia@users.noreply.github.com>
2019-10-08 10:16:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade
+apt update
+apt upgrade
 
 add-apt-repository ppa:nathan-renniewaldock/flux
-apt-get update
-apt-get install fluxgui
+apt update
+apt install fluxgui
 
-apt-get install git
+apt install htop
+
+add-apt-repository ppa:videolan/stable-daily
+apt update
+apt install vlc
+
+apt install git
+
+apt install vim
+
+apt install chromium-browser
+
+add-apt-repository ppa:noobslab/themes
+add-apt-repository ppa:noobslab/icons
+apt update
+apt install arc-theme arc-icons
 
 wget https://www.privateinternetaccess.com/installer/download_installer_linux 
