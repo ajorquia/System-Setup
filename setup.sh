144510edd7ad27600ff5fd943f3eeebb3db65a58
A.J <32173620+ajorquia@users.noreply.github.com>
2021-02-21 11:42:16 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,29 +3,26 @@ apt update
 apt upgrade
 
 add-apt-repository ppa:nathan-renniewaldock/flux
-apt update
-apt install fluxgui
-
-apt install htop
-
 add-apt-repository ppa:videolan/stable-daily
+
 apt update
+
+apt install fluxgui
+apt install htop
 apt install vlc
-
 apt install git
-
 apt install vim
-
 apt install chromium-browser
+
+##Nextcloud: https://github.com/nextcloud/desktop/releases/
 
 #No longer necessary with new Mint dark theme. See below to automatically set theme.
 #add-apt-repository ppa:noobslab/themes
 #add-apt-repository ppa:noobslab/icons
-#apt update
 #apt install arc-theme arc-icons
 
-sudo apt install snapd
-sudo snap install mailspring
+##sudo apt install snapd
+##sudo snap install mailspring
 
 #wget https://www.privateinternetaccess.com/installer/download_installer_linux 
 
