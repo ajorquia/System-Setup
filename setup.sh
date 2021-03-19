4e3064f47446f7142a102addbe4d5f5adaf1dbce
A.J <32173620+ajorquia@users.noreply.github.com>
2021-03-18 21:53:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ THEME=Mint-Y-Dark-Teal
 
 gsettings set org.cinnamon.desktop.wm.preferences theme '$THEME'
 gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-Black'
-gsettings set org.cinnamon.theme name 'THEME'
+gsettings set org.cinnamon.theme name '$THEME'
 gsettings set org.cinnamon.desktop.interface icon-theme '$THEME'
 gsettings set org.cinnamon.desktop.interface gtk-theme '$THEME'
 
